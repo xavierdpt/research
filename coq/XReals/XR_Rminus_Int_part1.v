3087249eeaccd9e9c8a47c17bee6d0a5d9bfe6f0
xavierdpt <xavierdpt>
2019-07-15 10:09:33 +0200

    .

--- coq/XReals/XR_Rminus_Int_part1.v
+++ coq/XReals/XR_Rminus_Int_part1.v
@@ -1,273 +1,9 @@
-Require Export XR_archimed.
-Require Export XR_base_fp.
-Require Export XR_base_Int_part.
-Require Export XR_bound.
-Require Export XR_completeness.
-Require Export XR_completeness_weak.
-Require Export XR_double.
-Require Export XR_double_var.
-Require Export XR_eq_IZR_R0.
-Require Export XR_eq_IZR.
-Require Export XR_f_equal_R.
-Require Export XR_for_base_fp.
-Require Export XR_fp_nat.
-Require Export XR_fp_R0.
 Require Export XR_frac_part.
-Require Export XR_INR_eq.
-Require Export XR_INR_IZR_INZ.
-Require Export XR_INR_le.
-Require Export XR_INR_lt_1.
-Require Export XR_INR_lt.
-Require Export XR_INR_not_0.
-Require Export XR_INR_pos.
-Require Export XR_INR.
-Require Export XR_inser_trans_R.
-Require Export XR_Int_part_INR.
-Require Export XR_Int_part.
-Require Export XR_is_lub.
-Require Export XR_is_upper_bound.
-Require Export XR_IZN.
-Require Export XR_IZR_eq.
-Require Export XR_IZR_le.
-Require Export XR_IZR_lt.
-Require Export XR_IZR_neq.
-Require Export XR_IZR.
-Require Export XR_le_0_IZR.
-Require Export XR_le_epsilon.
-Require Export XR_le_INR.
-Require Export XR_le_IZR_R1.
-Require Export XR_le_IZR.
-Require Export XR_le_O_IZR.
-Require Export XR_lt_0_INR.
-Require Export XR_lt_0_IZR.
-Require Export XR_lt_1_INR.
-Require Export XR_lt_INR_0.
-Require Export XR_lt_INR.
-Require Export XR_lt_IZR.
-Require Export XR_lt_O_IZR.
-Require Export XR_minus_INR.
-Require Export XR_minus_IZR.
-Require Export XR_mult_INR.
-Require Export XR_mult_IZR.
-Require Export XR_negreal.
-Require Export XR_nonnegreal.
-Require Export XR_nonposreal.
-Require Export XR_nonzeroreal.
-Require Export XR_not_0_INR.
-Require Export XR_not_0_IZR.
-Require Export XR_not_1_INR.
-Require Export XR_not_INR_O.
-Require Export XR_not_INR.
-Require Export XR_not_nm_INR.
-Require Export XR_not_O_INR.
-Require Export XR_not_O_IZR.
-Require Export XR_one_IZR_lt1.
-Require Export XR_one_IZR_r_R1.
-Require Export XR_opp_IZR.
-Require Export XR_plus_INR.
-Require Export XR_plus_IZR_NEG_POS.
-Require Export XR_plus_IZR.
-Require Export XR_plus_le_is_le.
-Require Export XR_plus_lt_is_lt.
-Require Export XR_pos_INR_nat_of_P.
-Require Export XR_pos_INR.
-Require Export XR_posreal.
-Require Export XR_pow_IZR.
-Require Export XR_pow.
-Require Export XR_prod_neq_R0.
-Require Export XR_R0_fp_O.
-Require Export XR_R0.
-Require Export XR_R1_neq_R0.
-Require Export XR_R1.
-Require Export XR_R2_neq_R0.
-Require Export XR_R2.
-Require Export XR_Rdichotomy.
-Require Export XR_Rdiv_lt_0_compat.
-Require Export XR_Rdiv_minus_distr.
-Require Export XR_Rdiv_plus_distr.
-Require Export XR_Rdiv.
-Require Export XR_Req_dec.
-Require Export XR_Req_EM_T.
-Require Export XR_Req_le_sym.
-Require Export XR_Req_le.
-Require Export XR_Rinv_0_lt_compat.
-Require Export XR_Rinv_1_lt_contravar.
-Require Export XR_Rinv_1.
-Require Export XR_Rinv_involutive.
-Require Export XR_Rinv_le_contravar.
-Require Export XR_Rinv_l_sym.
-Require Export XR_Rinv_lt_0_compat.
-Require Export XR_Rinv_lt_contravar.
-Require Export XR_Rinv_l.
-Require Export XR_Rinv_mult_distr.
-Require Export XR_Rinv_mult_simpl.
-Require Export XR_Rinv_neq_0_compat.
-Require Export XR_Rinv_r_simpl_l.
-Require Export XR_Rinv_r_simpl_m.
-Require Export XR_Rinv_r_simpl_r.
-Require Export XR_Rinv_r_sym.
-Require Export XR_Rinv_r.
-Require Export XR_Rinv.
-Require Export XR_Rle_0_1.
-Require Export XR_Rle_0_sqr.
-Require Export XR_Rle_antisym.
-Require Export XR_Rle_dec.
-Require Export XR_Rle_le_eq.
-Require Export XR_Rle_lt_0_plus_1.
-Require Export XR_Rle_lt_or_eq_dec.
-Require Export XR_Rle_lt_trans.
-Require Export XR_Rle_minus.
-Require Export XR_Rle_not_lt.
-Require Export XR_Rle_or_lt.
-Require Export XR_Rle_refl.
-Require Export XR_Rle_Rinv.
-Require Export XR_Rle_trans.
-Require Export XR_Rle.
-Require Export XR_Rlt_0_1.
-Require Export XR_Rlt_0_INR_2.
-Require Export XR_Rlt_0_sqr.
-Require Export XR_Rlt_asym.
-Require Export XR_Rlt_dec.
-Require Export XR_Rlt_dichotomy_converse.
-Require Export XR_Rlt_eq_compat.
-Require Export XR_Rlt_irrefl.
-Require Export XR_Rlt_le_dec.
-Require Export XR_Rlt_le_trans.
-Require Export XR_Rlt_le.
-Require Export XR_Rlt_lt_dec.
-Require Export XR_Rlt_minus.
-Require Export XR_Rlt_not_eq.
-Require Export XR_Rlt_not_le.
-Require Export XR_Rlt_or_le.
-Require Export XR_Rlt_plus_1.
-Require Export XR_Rlt_R0_R2.
-Require Export XR_Rlt_Rminus.
-Require Export XR_Rlt_trans.
-Require Export XR_Rlt.
-Require Export XR_Rminus_0_l.
-Require Export XR_Rminus_0_r.
-Require Export XR_Rminus_diag_eq.
-Require Export XR_Rminus_diag_uniq_sym.
-Require Export XR_Rminus_diag_uniq.
-Require Export XR_Rminus_eq_contra.
-Require Export XR_Rminus_le.
-Require Export XR_Rminus_lt.
-Require Export XR_Rminus_not_eq_right.
-Require Export XR_Rminus_not_eq.
-Require Export XR_Rminus.
-Require Export XR_Rmult_0_l.
-Require Export XR_Rmult_0_r.
-Require Export XR_Rmult_1_l.
-Require Export XR_Rmult_1_r.
-Require Export XR_Rmult_assoc.
-Require Export XR_Rmult_comm.
-Require Export XR_Rmult_eq_0_compat_l.
-Require Export XR_Rmult_eq_0_compat_r.
-Require Export XR_Rmult_eq_0_compat.
-Require Export XR_Rmult_eq_compat_l.
-Require Export XR_Rmult_eq_compat_r.
-Require Export XR_Rmult_eq_reg_l.
-Require Export XR_Rmult_eq_reg_r.
-Require Export XR_Rmult_integral_contrapositive_currified.
-Require Export XR_Rmult_integral_contrapositive.
-Require Export XR_Rmult_integral.
-Require Export XR_Rmult_le_0_lt_compat.
-Require Export XR_Rmult_le_compat_l.
-Require Export XR_Rmult_le_compat_neg_l.
-Require Export XR_Rmult_le_compat_r.
-Require Export XR_Rmult_le_compat.
-Require Export XR_Rmult_le_pos.
-Require Export XR_Rmult_le_reg_l.
-Require Export XR_Rmult_le_reg_r.
-Require Export XR_Rmult_lt_0_compat.
-Require Export XR_Rmult_lt_compat_l.
-Require Export XR_Rmult_lt_compat_r.
-Require Export XR_Rmult_lt_reg_l.
-Require Export XR_Rmult_lt_reg_r.
-Require Export XR_Rmult_minus_distr_l.
-Require Export XR_Rmult_neq_0_reg.
-Require Export XR_Rmult_ne.
-Require Export XR_Rmult_opp_opp.
-Require Export XR_Rmult_plus_distr_l.
-Require Export XR_Rmult_plus_distr_r.
-Require Export XR_Rmult.
-Require Export XR_Rnot_le_lt.
-Require Export XR_Rnot_lt_le.
-Require Export XR_Ropp_0.
-Require Export XR_Ropp_div.
-Require Export XR_Ropp_eq_0_compat.
-Require Export XR_Ropp_eq_compat.
-Require Export XR_Ropp_involutive.
-Require Export XR_Ropp_inv_permute.
-Require Export XR_Ropp_le_cancel.
 Require Export XR_Ropp_le_contravar.
-Require Export XR_Ropp_lt_cancel.
-Require Export XR_Ropp_lt_contravar.
-Require Export XR_Ropp_minus_distr_.
-Require Export XR_Ropp_minus_distr.
-Require Export XR_Ropp_mult_distr_l_reverse.
-Require Export XR_Ropp_mult_distr_l.
-Require Export XR_Ropp_mult_distr_r_reverse.
-Require Export XR_Ropp_mult_distr_r.
-Require Export XR_Ropp_neq_0_compat.
-Require Export XR_Ropp_plus_distr.
-Require Export XR_Ropp_Ropp_IZR.
-Require Export XR_Ropp.
-Require Export XR_Rplus_0_l.
-Require Export XR_Rplus_0_r_uniq.
-Require Export XR_Rplus_0_r.
-Require Export XR_Rplus_assoc.
-Require Export XR_Rplus_comm.
-Require Export XR_Rplus_eq_0_l.
-Require Export XR_Rplus_eq_compat_l.
-Require Export XR_Rplus_eq_compat_r.
-Require Export XR_Rplus_eq_R0.
-Require Export XR_Rplus_eq_reg_l.
-Require Export XR_Rplus_eq_reg_r.
-Require Export XR_Rplus_le_compat_l.
-Require Export XR_Rplus_le_compat_r.
-Require Export XR_Rplus_le_compat.
-Require Export XR_Rplus_le_le_0_compat.
-Require Export XR_Rplus_le_lt_0_compat.
-Require Export XR_Rplus_le_lt_compat.
-Require Export XR_Rplus_le_reg_l.
-Require Export XR_Rplus_le_reg_pos_r.
-Require Export XR_Rplus_le_reg_r.
-Require Export XR_Rplus_lt_0_compat.
-Require Export XR_Rplus_lt_compat_l.
-Require Export XR_Rplus_lt_compat_r.
-Require Export XR_Rplus_lt_compat.
-Require Export XR_Rplus_lt_le_0_compat.
-Require Export XR_Rplus_lt_le_compat.
-Require Export XR_Rplus_lt_pos.
-Require Export XR_Rplus_lt_reg_l.
-Require Export XR_Rplus_lt_reg_pos_r.
-Require Export XR_Rplus_lt_reg_r.
-Require Export XR_Rplus_minus.
-Require Export XR_Rplus_ne.
-Require Export XR_Rplus_opp_l.
-Require Export XR_Rplus_opp_r_uniq.
-Require Export XR_Rplus_opp_r.
-Require Export XR_Rplus_sqr_eq_0_l.
-Require Export XR_Rplus_sqr_eq_0.
-Require Export XR_Rplus.
-Require Export XR_Rsqr_0_uniq.
-Require Export XR_Rsqr_0.
-Require Export XR_Rsqr.
-Require Export XR_Rtotal_order.
-Require Export XR_R.
-Require Export XR_single_z_r_R1.
-Require Export XR_S_INR.
-Require Export XR_S_O_plus_INR.
-Require Export XR_succ_IZR.
-Require Export XR_sum_inequa_Rle_lt.
-Require Export XR_tech_Rplus.
-Require Export XR_tech_single_z_r_R1.
-Require Export XR_tech_up.
-Require Export XR_total_order_T.
 Require Export XR_up_tech.
-Require Export XR_up.
-Require Export XR_Z_R_minus.
+Require Export XR_IZR_eq.
+Require Export XR_Rplus_lt_le_compat.
+
 Local Open Scope R_scope.
 
 Lemma Rminus_Int_part1 : forall r1 r2:R,
@@ -277,82 +13,85 @@ Proof.
   intros x y.
   unfold frac_part.
   unfold Int_part.
-  repeat rewrite minus_IZR.
-  unfold Rminus.
-  repeat rewrite Ropp_plus_distr.
-  repeat rewrite Ropp_involutive.
-  simpl.
   intro h.
+  set (u:=up(x-y)).
+  apply eq_IZR.
+  repeat rewrite minus_IZR.
+  repeat rewrite minus_IZR in h.
+  simpl.
+  unfold Rminus.
+  rewrite Ropp_plus_distr.
+  rewrite Ropp_involutive.
+  repeat rewrite Rplus_assoc.
+  rewrite (Rplus_comm (-R1)).
+  repeat rewrite Rplus_assoc.
+  rewrite Rplus_opp_r.
+  rewrite Rplus_0_r.
+  unfold Rminus in h.
+  repeat rewrite Ropp_plus_distr in h.
+  repeat rewrite Ropp_involutive in h.
   repeat rewrite <- Rplus_assoc in h.
   apply Rplus_le_reg_r in h.
-  repeat rewrite <- Z.add_opp_r.
-  rewrite Z.opp_add_distr.
-  rewrite Z.opp_involutive.
-  repeat rewrite Zplus_assoc_reverse.
-  rewrite (Zplus_comm (-(1)))%Z.
-  repeat rewrite Zplus_assoc_reverse.
-  rewrite Z.add_opp_diag_r.
-  rewrite Z.add_0_r.
-
-  destruct (archimed (x+-y)) as [ hl hu ].
-
-  set (xy := up(x+-y)).
-  fold xy in hl.
-  fold xy in hu.
-
-  assert (tu := tech_up).
-  specialize (tu (x+-y)).
-  fold xy in tu.
-
-  apply eq_IZR.
-  repeat rewrite plus_IZR.
-  rewrite opp_IZR.
-  simpl.
-  rewrite opp_IZR.
-
   apply Rplus_eq_reg_r with R1.
-  repeat rewrite Rplus_assoc.
-  rewrite Rplus_opp_l.
-  rewrite Rplus_0_r.
-  symmetry.
-  rewrite <- opp_IZR.
-  change R1 with (IZR 1%Z).
-  rewrite <- plus_IZR.
-  rewrite <- plus_IZR.
-  apply IZR_eq.
+  rewrite Rplus_assoc, Rplus_opp_l, Rplus_0_r.
+  apply Ropp_le_contravar in h.
+  repeat rewrite Ropp_plus_distr in h.
+  repeat rewrite Ropp_involutive in h.
+  rewrite (Rplus_comm (-x)) in h.
+  rewrite (Rplus_comm (-y)) in h.
+
+  assert (hax:=archimed x).
+  assert (hay:=archimed y).
+
+  destruct hax as [ haxl haxr ].
+  destruct hay as [ hayl hayr ].
 
   assert (ut:=up_tech).
-  specialize (ut (x+-y)).
-  fold xy in ut.
-  rewrite Zplus_assoc.
+
+  specialize (ut (x-y)).
+  symmetry.
+
+  change R1 with (IZR 1).
+  rewrite <- opp_IZR.
+  repeat rewrite <- plus_IZR.
+  apply IZR_eq.
+  fold u in ut.
+
+  specialize (ut (up x - up y))%Z.
   apply ut.
 
   {
-rewrite plus_IZR.
-rewrite opp_IZR.
-apply Rplus_le_reg_l with (- IZR (up x)).
-repeat rewrite <- Rplus_assoc.
-rewrite Rplus_opp_l.
-rewrite Rplus_0_l.
-apply Rplus_le_reg_l with y.
-pattern y at 2 ; rewrite Rplus_comm.
-repeat rewrite Rplus_assoc.
-rewrite Rplus_opp_l, Rplus_0_r.
-rewrite (Rplus_comm _ x).
-exact h.
-}
-{
-repeat rewrite plus_IZR.
-rewrite opp_IZR.
-simpl.
-clear tu ut.
-
- 
-
-
-
-
-
-
-
-
+    rewrite minus_IZR.
+    apply Rplus_le_reg_r with (-x).
+    unfold Rminus.
+    repeat rewrite Rplus_assoc.
+    rewrite (Rplus_comm x).
+    repeat rewrite Rplus_assoc.
+    rewrite Rplus_opp_l.
+    rewrite Rplus_0_r.
+    apply Rplus_le_reg_r with (IZR (up y)).
+    repeat rewrite Rplus_assoc.
+    rewrite (Rplus_comm (- IZR (up y))).
+    repeat rewrite Rplus_assoc.
+    rewrite Rplus_opp_r.
+    rewrite Rplus_0_r.
+    rewrite (Rplus_comm (-y)).
+    exact h.
+  }
+  {
+    rewrite plus_IZR.
+    rewrite minus_IZR.
+    unfold Rminus.
+    repeat rewrite Rplus_assoc.
+    apply Rplus_lt_le_compat.
+    { exact haxl. }
+    {
+      apply Rplus_le_reg_l with (IZR (up y)).
+      repeat rewrite <- Rplus_assoc.
+      rewrite Rplus_opp_r.
+      rewrite Rplus_0_l.
+      unfold Rminus in hayr.
+      exact hayr.
+    }
+  }
+Qed.
